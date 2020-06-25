359fa3c92c08ad10b896c8c3ac27c8ed880e3b35
Andre Macedo <andremacedo.pv@gmail.com>
2020-06-24 22:27:58 -0300

    Criação da controller de tipos de bebidas e htmls simples relacionados

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'application#homepage'
-  resource :addresses, :users, :products
+  resources :addresses, :users, :products, :drink_types
 
   #Creating and renaming the main session's paths
   resource :sessions
