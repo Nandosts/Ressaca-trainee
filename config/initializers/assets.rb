d10051ef7306cf4e974b8f1b1592f805adf83523
Nandosts <fernando.jsts@gmail.com>
2020-07-02 12:49:02 -0300

    visual em colunas e Card melhorado

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( navbar.scss )
 Rails.application.config.assets.precompile += %w( login.scss )
+Rails.application.config.assets.precompile += %w( card.scss )
