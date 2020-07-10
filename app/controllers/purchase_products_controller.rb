fd5cea23b2d90c75b304c0dd6697de3becb81134
Felipe Lima <felipesoueu@live.com>
2020-07-10 19:20:43 -0300

    Bug dos produtos duplicados corrigido

--- app/controllers/purchase_products_controller.rb
+++ app/controllers/purchase_products_controller.rb
@@ -3,23 +3,55 @@ class PurchaseProductsController < ApplicationController
   before_action :require_login
 
   def create
+
+    # Checagem se o usuário é maior de idade
     if check_age(Product.find(params[:id]), calculate_age(current_user.birthday))
-      purchase_product = PurchaseProduct.new(purchase_id: current_user.purchases.find_by(bought: false).id,
-                                             product_id: params[:id],
-                                             quantity: params[:quantity])
-      add_to_price_tag(Product.find(params[:id].to_f), params[:quantity].to_f)
-      begin
-        purchase_product.save!
-        flash[:notice] = 'Produto adicionado ao carrinho com sucesso!'
-        redirect_to cart_path
-      rescue => err
-        print err
-        remove_from_price_tag(Product.find(params[:id].to_f), params[:quantity].to_f)
-        flash[:notice] = 'Algo deu errado!'
-        redirect_to product_path(params[:id])
+
+      # Buscando se o produto já está no carrihno
+      purchase_product = current_user.purchases.find_by(bought: false).purchase_products.find_by(product_id: params[:id])
+
+      # Produto é novo no carrinho
+      if purchase_product == nil
+        new_product_on_cart
+        # Produto já está no carrinho
+      else
+        increase_product_on_cart(purchase_product)
       end
     else
       redirect_to root_path
+    end
+  end
+
+  # Adicionoa um produto ao carrinho
+  def new_product_on_cart
+    purchase_product = PurchaseProduct.new(purchase_id: current_user.purchases.find_by(bought: false).id,
+                                           product_id: params[:id],
+                                           quantity: params[:quantity])
+    begin
+      purchase_product.save!
+      add_to_price_tag(Product.find(params[:id].to_f), params[:quantity].to_f)
+      flash[:notice] = 'Produto adicionado ao carrinho com sucesso!'
+      redirect_to cart_path
+    rescue => err
+      print err
+      flash[:notice] = 'Algo deu errado!'
+      redirect_to product_path(params[:id])
+    end
+  end
+
+  # Aumenta a quantidade de um item no carrinho
+  def increase_product_on_cart(purchase_product)
+
+    new_quantity = purchase_product.quantity + params[:quantity].to_f
+    begin
+      purchase_product.update!(quantity: new_quantity)
+      add_to_price_tag(Product.find(params[:id].to_f), params[:quantity].to_f)
+      flash[:notice] = 'Produto adicionado ao carrinho com sucesso!'
+      redirect_to cart_path
+    rescue => err
+      print err
+      flash[:notice] = 'Algo deu errado!'
+      redirect_to product_path(params[:id])
     end
   end
 
@@ -84,6 +116,7 @@ class PurchaseProductsController < ApplicationController
     end
   end
 
+  # Retorna false se a idade for menor de 18 e o produto for alcoolico
   def check_age (product, age)
     if age < 18 and product.drink_type.alcoholic
       flash[:notice] = 'Você não é maior do idade!'
